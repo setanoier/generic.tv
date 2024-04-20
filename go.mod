948f1907ad4763fd0ace3ce21bdb3a5bbfb147a1
setanoier <setanoier@gmail.com>
2024-04-21 00:25:35 +0300

    ping pong!

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.22.1
+
+require github.com/gempir/go-twitch-irc/v4 v4.0.0
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gempir/go-twitch-irc/v4"
+	"log"
+	"os"
+)
+
+func readStringFromFile(filename string) string {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	strData := string(data)
+	return strData
+}
+
+func main() {
+	accessToken := readStringFromFile(".client") // access token is oauth token of bot account
+	client := twitch.NewClient("setanoier", accessToken)
+
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		fmt.Println(message.Message)
+		if message.Message == "!ping" {
+			client.Say(message.Channel, "pong!\n")
+		}
+	})
+
+	client.Join("setanoier")
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
