8ec9c86c805bfd6f0a297b3a6e4b06a26ef8704a
setanoier <setanoier@gmail.com>
2024-06-26 12:58:40 +0300

    feat: Add functionality to get a link to the latest YouTube video

---
+++ internal/youtube/youtube.go
@@ -0,0 +1,70 @@
+package youtube
+
+import (
+	"encoding/json"
+	"fmt"
+	"generic.tv/internal/utils"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+// Video represents the structure of a video item in the YouTube API response
+type Video struct {
+	ID struct {
+		VideoID string `json:"videoId"`
+	} `json:"id"`
+	Snippet struct {
+		Title string `json:"title"`
+	} `json:"snippet"`
+}
+
+// YouTubeResponse represents the structure of the response from the YouTube API
+type YouTubeResponse struct {
+	Items []Video `json:"items"`
+}
+
+// FetchLatestVideoLink returns the link to the latest video from the specified YouTube channel
+func FetchLatestVideoLink() (string, string, error) {
+	apiKey := utils.ReadStringFromFile("../data/gapi.txt")
+	channelID := "UCGlYKd-FR4g0Tp4wF6_wxig"
+	baseURL := "https://www.googleapis.com/youtube/v3/search"
+	params := url.Values{}
+	params.Add("part", "snippet")
+	params.Add("channelId", channelID)
+	params.Add("order", "date")
+	params.Add("type", "video")
+	params.Add("maxResults", "1")
+	params.Add("key", apiKey)
+
+	apiURL := fmt.Sprintf("%s?%s", baseURL, params.Encode())
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return "", "", fmt.Errorf("error making HTTP request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		return "", "", fmt.Errorf("unexpected response status: %s, body: %s", resp.Status, string(bodyBytes))
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", "", fmt.Errorf("error reading response body: %v", err)
+	}
+
+	var ytResponse YouTubeResponse
+	if err := json.Unmarshal(bodyBytes, &ytResponse); err != nil {
+		return "", "", fmt.Errorf("error unmarshalling JSON response: %v", err)
+	}
+
+	if len(ytResponse.Items) == 0 {
+		return "", "", fmt.Errorf("no videos found for channel ID %s", channelID)
+	}
+
+	latestVideo := ytResponse.Items[0]
+	videoTitle := latestVideo.Snippet.Title
+	videoURL := fmt.Sprintf("https://www.youtube.com/watch?v=%s", latestVideo.ID.VideoID)
+	return videoTitle, videoURL, nil
+}
