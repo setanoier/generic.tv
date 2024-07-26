7f9e0a06c1eb598de6853450dcc144d9ba53d6d8
setanoier <setanoier@gmail.com>
2024-07-26 18:35:59 +0300

    feat: add db support

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"database/sql"
 	"log"
 	"os"
 )
@@ -26,4 +27,43 @@ func GetWeatherEmoji(temp int) string {
 	default:
 		return "???"
 	}
+}
+
+func OpenDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "../storage/streamers.db")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func CreateTable(db *sql.DB) error {
+	stmt := `
+		CREATE TABLE IF NOT EXISTS streamers (
+			twitch TEXT PRIMARY KEY,
+			oauth_token TEXT,
+			gapi TEXT,
+			mapi TEXT
+		)
+	`
+
+	_, err := db.Exec(stmt)
+	return err
+}
+
+func Contains(db *sql.DB, twitch string) (bool, error) {
+	var exists bool
+	query := `SELECT EXISTS(SELECT 1 FROM streamers WHERE twitch = ? LIMIT 1)`
+	err := db.QueryRow(query, twitch).Scan(&exists)
+	return exists, err
+}
+
+func Insert(db *sql.DB, twitch, oauthToken, gapi, mapi string) error {
+	stmt, err := db.Prepare(`INSERT INTO streamers (twitch, oauth_token, gapi, mapi) 
+	VALUES (?, ?, ?, ?)`)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(twitch, oauthToken, gapi, mapi)
+	return err
 }
